a9430a2e0ae25e512d5bd41196af9aa9c505e211
MehdiTheCoder <mehdidoukkali57@gmail.com>
2025-03-13 19:59:02 +0000

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'launch_page/intro_page.dart';
+import 'package:lbrikol/auth/login.dart';
+import 'package:lbrikol/auth/register.dart';
+import 'package:lbrikol/student_form/academic_info.dart';
+import 'package:lbrikol/student_form/skills_services.dart';
+import 'package:lbrikol/student_form/basic_info_student.dart';
+import 'package:lbrikol/student_interfaces/home_page_gigs.dart';
 
 // The home page will be first run
 void main() {
-  runApp(const Lbrikol());
+  runApp(const MyApp());
 }
 
 class Lbrikol extends StatefulWidget {
@@ -16,9 +22,28 @@ class Lbrikol extends StatefulWidget {
 class _LbrikolState extends State<Lbrikol> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const AcademicInformationScreen(),
+      routes: {
+        '/intro': (context) => const OnboardingScreen(), // Keep original
+        '/login': (context) => LogInPage(), // Corrected from LoginScreen
+        '/register': (context) => const RegisterPage(), // Corrected from RegisterScreen
+        '/basicInfoStudent': (context) => const BasicInfoStudentPage(), // Corrected from BasicInfoStudentScreen
+        '/academicInfo': (context) => const AcademicInformationScreen(),
+        '/skillsServices': (context) => const SkillsAndServicesScreen(),
+        '/home': (context) => const MainScreen(), // Ensure this class exists
+      },
     );
   }
 }
+
+
+
+
+
+
+
+
+
